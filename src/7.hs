9b7a47bfae47d6054a826aea66360be10e7225a7
Cameron Mann <cameron.mann@cybera.ca>
2020-12-08 12:48:25 -0700

    Complete day 7 solution

--- src/7.hs
+++ src/7.hs
@@ -1,13 +1,11 @@
 import Data.Map ((!))
 import qualified Data.Map as Map
 import qualified Data.Set as Set
-import Text.Read
 import Text.Parsec
 
 rules = Map.fromList <$> sepEndBy rule newline
 rule = (,) <$> (bag <* string " contain ") <*> (contents <* char '.')
 contents = [] <$ string "no other bags" <|> sepBy ((,) <$> many digit <* space <*> bag) (string ", ")
--- num = read <$> many digit :: Int
 bag = (++) <$> many lower <* string " " <*> many lower <* space <* many lower
 
 canHold rules color =
@@ -16,12 +14,7 @@ canHold rules color =
     colors = Map.keys $ Map.filter (\xs -> elem color $ map snd xs) rules
 
 holds rules color =
-  -- total + (sum . map (holds rules . snd) $ rule)
-  sum $ map (\(i, j) -> i * j) foo
-  where
-    foo = map (\(i, c) -> (read i, holds rules c)) rule
-    -- total = sum . map (read . fst) $ rule
-    rule = rules ! color
+  sum . map (\(i, c) -> read i * (holds rules c + 1)) $ rules ! color
 
 main = do
   input <- getContents
