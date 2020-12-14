b57361960f9ac7ba74ef929ff42f228e580a60cd
Cameron Mann <cameron.mann@cybera.ca>
2020-12-13 19:55:38 -0700

    Simplify day 13 solutions

--- src/13.hs
+++ src/13.hs
@@ -1,21 +1,18 @@
-import Data.Maybe
 import Data.List
 import Data.List.Split
+import Data.Maybe
+import Data.Ord
 import Text.Read
 
-part1 ids ts = id * (best - ts)
-  where
-    times = map (\x -> x * (1 + quot ts x)) ids
-    best = minimum times
-    (Just i) = elemIndex best times
-    id = ids !! i
+part1 ids ts = fst $ minimumBy (comparing snd) deltas
+  where deltas = [(id*delta,delta) | id <- ids, let delta = id - mod ts id]
 
-part2 ((_,id):xs) = fst $ foldl' lcm (id,id) xs
+part2 ((id,off):xs) = fst $ foldl' lcm (id,id) xs
   where
-    lcm (ts,inc) (off,id) =
+    lcm (ts,inc) (id,off) =
       case mod (ts+off) id of
         0 -> (ts,inc*id)
-        _ -> lcm (ts+inc,inc) (off,id)
+        _ -> lcm (ts+inc,inc) (id,off)
 
 main = do
   input <- lines <$> getContents
@@ -23,5 +20,5 @@ main = do
   let ts = read $ head input
   let ids = catMaybes parsed
   print $ part1 ids ts
-  let offsets = [(off,id) | (off,Just id) <- zip [0..] parsed]
+  let offsets = [(id,off) | (off,Just id) <- zip [0..] parsed]
   print $ part2 offsets
